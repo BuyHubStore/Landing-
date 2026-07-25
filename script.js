5bfc7a2c7fb226b63c5eaa5911008c17a253ee4f
BuyHubStore <levcukstas8@gmail.com>
2026-07-25 09:20:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.querySelectorAll(".btn").forEach(button => {
 
+button.addEventListener("click", () => {
+
+button.style.transform = "scale(0.96)";
+
+setTimeout(() => {
+
+button.style.transform = "";
+
+},150);
+
+});
+
+});
